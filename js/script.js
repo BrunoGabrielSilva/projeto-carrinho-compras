1527191082b6539f9bfa048eab76cd89b41b13df
Bruno Gabriel <bruno.gabrielsilva@gmail.com>
2025-04-16 20:57:41 -0300

    ProjetoFinalV1

--- js/script.js
+++ js/script.js
@@ -10,14 +10,33 @@ function adicionarItem(item) {
     valorTotal[item] = Number.parseFloat(valorProduto[item]) * qtd[item];
     quantidade.innerHTML = qtd[item];
     total.innerHTML = valorTotal[item].toFixed(2);
+    ValorCompra();
 
 }
 
 function removerItem(item) {
+
     if (qtd[item] > 0) {
         qtd[item] -= 1;
+        var quantidade = document.getElementById('quantidade'+ item);
+        var total = document.getElementById('total'+ item);
         valorTotal[item] = Number.parseFloat(valorProduto[item]) * qtd[item];
         quantidade.innerHTML = qtd[item];
         total.innerHTML = valorTotal[item].toFixed(2);
+        ValorCompra();
+
     }
+}
+
+function ValorCompra(){
+    var ValorTotalCompra = document.getElementById('ValorTotalCompra');
+    var valor = 0;
+
+    for (var i = 0; i < valorTotal.length; i++){
+        valor += valorTotal[i];
+    }
+
+    ValorTotalCompra.innerHTML = valor.toFixed(2);
+
+
 }
